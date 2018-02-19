1bf4ca6c4c61c8c7298ff53e658660e4bd337029
Nael <nael.ait@gmail.com>
2018-02-19 10:46:11 +0100

    Almost done

--- formulaire.php
+++ formulaire.php
@@ -1,51 +1,42 @@
 <?php
-  if (isset($_POST["tache"])) {
-    if (empty($_POST["tache"])){
-      echo "Veuillez indiqué une tâche à ajouter.";
-    }
-    else {
-      //lecture du fichier json
-      $read = file_get_contents('todo.json');
-      $decode = json_decode($read, true);
-      //Si json vide, créer tableau
-      if (empty($decode)){
-        $decode = array();
-      }
-      //
-      $newTache = $_POST['tache'];
-      $newTache = htmlspecialchars($newTache);
-      $i = count($decode);
-      $decode[$i] = $newTache;
-      $objet = (object) $decode;
-      $encode = json_encode($objet, true);
-      $write = file_put_contents('todo.json', $encode);
-    }
+$getJson = file_get_contents('todo.json');
+$readJson = json_decode($getJson, true);
+if (empty($readJson)) {
+    $json = array(
+    "Todo" => array(),
+    "Archive" => array(),
+  );
+} else {
+    $json = $readJson;
+}
+if (isset($_POST['tache'])) {
+    array_push($json['Todo'], $_POST['tache']);
+    $jsonEncode = json_encode($json, JSON_PRETTY_PRINT);
+    file_put_contents('todo.json', $jsonEncode);
+}
+if (isset($_POST['check'])) {
+  foreach ($_POST['check'] as $key => $value) {
+    array_push($json['Archive'], $value);
+    $jsonEncode = json_encode($json, JSON_PRETTY_PRINT);
+    file_put_contents('todo.json', $jsonEncode);
   }
-?>
+}
 
+
+
+
+?>
+</form>
 <p>A FAIRE</p>
-<?php
-  $display = file_get_contents('todo.json');
-  $show = json_decode($display, true);
-  if (!empty($show)){
-    foreach ($show as $task) {
-?>
-  <form method="POST">
-  <div> <input type='checkbox' name='check[]' value="<?=$task?>"> <?=$task?>  </div>
-
-  <?php
-    }
-  }
-  if(isset($_POST['check'])){
-    foreach ($_POST['check'] as $save) {
-      var_dump($show);
-    }
-  }
-?>
-  <div> <input type="submit" value="Enregistrer"> </div>
+<form method="POST">
+  <?php foreach ($json['Todo'] as $key => $value) {
+    ?>
+  <div><input type="checkbox" name="check[]" value="<?=$value?>"> <?=$value?></div>
+    <?php
+} ?>
+<input type="submit" name="" value="Archiver">
 </form>
 <p>ARCHIVE</p>
-
 
 <h1>Ajouter une tâche</h1>
 
